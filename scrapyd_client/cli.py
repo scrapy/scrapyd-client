b9ba79906be2ba0a1911ee4a1d228831c78fc453
Frank Sachsenheim <funkyfuture@riseup.net>
2017-02-02 15:33:30 +0100

    Calls 'deploy.main' for deploy-subcommand and prints help on no command

--- scrapyd_client/cli.py
+++ scrapyd_client/cli.py
@@ -13,6 +13,12 @@ DEFAULT_TARGET_URL = 'http://localhost:6800'
 ISSUE_TRACKER_URL = 'https://github.com/scrapy/scrapyd-client/issues'
 
 
+def deploy(args):
+    from scrapyd_client import deploy
+    sys.argv.pop(1)
+    deploy.main()
+
+
 def projects(args):
     _projects = get_projects(args.target)
     if _projects:
@@ -28,11 +34,25 @@ def parse_cli_args(args, cfg):
     mainparser.add_argument('-t', '--target', default=target_default,
                         help="Specifies the Scrapyd's API base URL.")
 
+    description = 'Deploys a Scrapy project to a Scrapyd instance. ' \
+                  'For help on this command, invoke `scrapyd-deploy`.'
+    parser = subparsers.add_parser('deploy', description=description)
+    parser.set_defaults(action=deploy)
 
     description = 'Lists all projects deployed on a Scrapyd instance.'
     parser = subparsers.add_parser('projects', description=description)
     parser.set_defaults(action=projects)
 
+    # TODO remove next two lines when 'deploy' is moved to this module
+    parsed_args, _ = mainparser.parse_known_args(args)
+    if getattr(parsed_args, 'action', None) is not deploy:
+        parsed_args = mainparser.parse_args(args)
+
+    if not hasattr(parsed_args, 'action'):
+        mainparser.print_help()
+        raise SystemExit(0)
+
+    return parsed_args
 
 
 
