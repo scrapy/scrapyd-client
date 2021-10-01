41c587536164171ef509f41f3eb2ace91c468466
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-10-01 17:24:08 -0400

    test: Add tests for --list-targets

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+import scrapy.utils.conf as conf
+
+
+@pytest.fixture(autouse=True)
+def only_closest_scrapy_cfg(monkeypatch):
+    """
+    Avoids a developer's own configuration files interfering with tests.
+    """
+    def get_sources(use_closest=True):
+        if use_closest:
+            return [conf.closest_scrapy_cfg()]
+        return []
+
+    monkeypatch.setattr(conf, 'get_sources', get_sources)
