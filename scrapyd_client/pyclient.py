8c97eaae82c1a41b0e15b6524aa45ec53e1b7b26
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-05-15 17:18:49 -0400

    feat: Review PR: Remove error-handling that creates extra API requests; Remove "all" functionality; Simplify docstrings.

--- scrapyd_client/pyclient.py
+++ scrapyd_client/pyclient.py
@@ -22,40 +22,68 @@ class ScrapydClient:
         self.auth = get_auth(url=self.url, username=username, password=password)
 
     def projects(self, pattern: str = "*") -> list[str]:
-        response = self._get("listprojects")
-        return fnmatch.filter(response["projects"], pattern)
+        """
+        Return the projects matching a pattern (if provided).
+
+        :param pattern: The `pattern <https://docs.python.org/3/library/fnmatch.html>`__ for the projects to match
+        :return: The "projects" value of the API response, filtered by the pattern (if provided).
+
+        .. seealso:: `listprojects.json <https://scrapyd.readthedocs.io/en/latest/api.html#listprojects-json>`__
+        """
+        return fnmatch.filter(self._get("listprojects")["projects"], pattern)
 
     def spiders(self, project: str, pattern: str = "*") -> list[str]:
-        response = self._get("listspiders", params={"project": project})
-        return fnmatch.filter(response["spiders"], pattern)
+        """
+        Return the spiders matching a pattern (if provided).
+
+        :param pattern: The `pattern <https://docs.python.org/3/library/fnmatch.html>`__ for the spiders to match
+        :return: The "spiders" value of the API response, filtered by the pattern (if provided).
+
+        .. seealso:: `listspiders.json <https://scrapyd.readthedocs.io/en/latest/api.html#listspiders-json>`__
+        """
+        return fnmatch.filter(self._get("listspiders", {"project": project})["spiders"], pattern)
 
     def jobs(self, project: str) -> dict:
-        return self._get("listjobs", params={"project": project})
+        """
+        :return: The unmodified API response.
+
+        .. seealso:: `listjobs.json <https://scrapyd.readthedocs.io/en/latest/api.html#listjobs-json>`__
+        """
+        return self._get("listjobs", {"project": project})
 
     def daemonstatus(self) -> dict:
         """
-        Get the status of the Scrapyd daemon.
-        :return: JSON response from the Scrapyd daemon status endpoint.
+        :return: The unmodified API response.
+
+        .. seealso:: `daemonstatus.json <https://scrapyd.readthedocs.io/en/latest/api.html#daemonstatus-json>`__
         """
         return self._get("daemonstatus")
 
     def versions(self, project: str) -> list[str]:
         """
-        List versions for a given project.
-        :param project: Name of the project.
-        :return: List of versions for the project.
+        :return: The "versions" value of the API response.
+
+        .. seealso:: `listversions.json <https://scrapyd.readthedocs.io/en/latest/api.html#listversions-json>`__
         """
-        params = {"project": project}
-        response = self._get("listversions", params)
-        return response.get("versions", [])
+        return self._get("listversions", {"project": project})["versions"]
 
     def schedule(self, project: str, spider: str, args: list[tuple[str, str]] | None = None) -> str:
+        """
+        :return: The "jobid" value of the API response.
+
+        .. seealso:: `schedule.json <https://scrapyd.readthedocs.io/en/latest/api.html#schedule-json>`__
+        """
         if args is None:
             args = []
-        response = self._post("schedule", data=[*args, ("project", project), ("spider", spider)])
-        return response["jobid"]
+
+        return self._post("schedule", data=[*args, ("project", project), ("spider", spider)])["jobid"]
 
     def status(self, jobid: str, project: str | None = None) -> dict:
+        """
+        :return: The unmodified API response.
+
+        .. seealso:: `status.json <https://scrapyd.readthedocs.io/en/latest/api.html#status-json>`__
+        """
         params = {"job": jobid}
         if project is not None:
             params["project"] = project
@@ -64,61 +92,32 @@ class ScrapydClient:
 
     def delproject(self, project: str) -> dict:
         """
-        Delete a project.
-        :param project: Name of the project.
-        :return: JSON response from the Scrapyd delete project endpoint.
-        :raises ErrorResponse if the project is not found.
+        :return: The unmodified API response.
+
+        .. seealso:: `delproject.json <https://scrapyd.readthedocs.io/en/latest/api.html#delproject-json>`__
         """
-        if project not in self.projects():
-            raise ErrorResponse(f"Project {project} not found.")
         return self._post("delproject", data={"project": project})
 
     def delversion(self, project: str, version: str) -> dict:
         """
-        Delete a specific version of a project.
-        :param project: Name of the project.
-        :param version: Version to delete. Can be "all" to delete all versions.
-        :return: JSON response from the Scrapyd delete version endpoint.
-        :raises: ErrorResponse if the project or version is not found.
-        """
-        if project not in self.projects():
-            raise ErrorResponse(f"Project {project} not found.")
-        if version == "all":
-            versions = self.versions(project)
-            for ver in versions:
-                self._post("delversion", data={"project": project, "version": ver})
-            return {"status": "ok", "message": "All versions deleted."}
-        if version not in self.versions(project):
-            raise ErrorResponse(f"Version {version} not found in project {project}.")
+        :return: The unmodified API response.
 
+        .. seealso:: `delversion.json <https://scrapyd.readthedocs.io/en/latest/api.html#delversion-json>`__
+        """
         return self._post("delversion", data={"project": project, "version": version})
 
     def cancel(self, project: str, jobid: str) -> dict:
         """
-        Cancel a running job or all running jobs.
-        :param project: Name of the project.
-        :param jobid: ID of the job to cancel or "all" to cancel all jobs.
-        :return: JSON response from the Scrapyd cancel job endpoint.
-        :raises: ErrorResponse if the project or job is not found.
+        :return: The unmodified API response.
+
+        .. seealso:: `cancel.json <https://scrapyd.readthedocs.io/en/latest/api.html#cancel-json>`__
         """
-        if project not in self.projects():
-            raise ErrorResponse(f"Project {project} not found.")
-
-        running_jobs = self.jobs(project)["running"]
-        if jobid == "all":
-            responses = []
-            for job in running_jobs:
-                responses.append(self._post("cancel", data={"project": project, "job": job}))
-            return {"status": "ok", "responses": responses}
-
-        if jobid not in running_jobs:
-            raise ErrorResponse(f"Job {jobid} not found in project {project}.")
-
         return self._post("cancel", data={"project": project, "job": jobid})
 
     def _get(self, basename: str, params=None):
         if params is None:
             params = {}
+
         return _process_response(
             requests.get(f"{self.url}/{basename}.json", params=params, headers=HEADERS, auth=self.auth)
         )
